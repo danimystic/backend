d863132922e87c32193a3b942729380efc2ca7d8
danimystic <153471424+danimystic@users.noreply.github.com>
2023-12-11 15:52:03 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: 'dan-octa.online',
   port: 3306,
   user: 'dano4196_dan',
   password: 'Mamanx724@anzuui',
